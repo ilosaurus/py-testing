2412610852ba1f77343bb8938a613647d0087779
muhilhams <muhilhamsyarifuddin@gmail.com>
2020-11-22 22:28:11 +0700

    inital commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,  request
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    return 'User Address : '+request.remote_addr+'.\n Hits : '+format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
